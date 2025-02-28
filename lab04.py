9b752071ec4751e12f9c5791df5decb7c9d2814a
yuchienchen <yuchienc@yahoo.com>
2025-02-28 11:57:47 -0600

    Q5 still working on it

--- lab04.py
+++ lab04.py
@@ -83,22 +83,26 @@ def get_lon(city):
     return city[2]
 
 
-# def closer_city(lat, lon, city_a, city_b):
-#     """
-#     Returns the name of either city_a or city_b, whichever is closest to
-#     coordinate (lat, lon). If the two cities are the same distance away
-#     from the coordinate, consider city_b to be the closer city.
+def closer_city(lat, lon, city_a, city_b):
+    """
+    Returns the name of either city_a or city_b, whichever is closest to
+    coordinate (lat, lon). If the two cities are the same distance away
+    from the coordinate, consider city_b to be the closer city.
 
-#     >>> berkeley = make_city('Berkeley', 37.87, 112.26)
-#     >>> stanford = make_city('Stanford', 34.05, 118.25)
-#     >>> closer_city(38.33, 121.44, berkeley, stanford)
-#     'Stanford'
-#     >>> bucharest = make_city('Bucharest', 44.43, 26.10)
-#     >>> vienna = make_city('Vienna', 48.20, 16.37)
-#     >>> closer_city(41.29, 174.78, bucharest, vienna)
-#     'Bucharest'
-#     """
-#     "*** YOUR CODE HERE ***"
+    >>> berkeley = make_city('Berkeley', 37.87, 112.26)
+    >>> stanford = make_city('Stanford', 34.05, 118.25)
+    >>> closer_city(38.33, 121.44, berkeley, stanford)
+    'Stanford'
+    >>> bucharest = make_city('Bucharest', 44.43, 26.10)
+    >>> vienna = make_city('Vienna', 48.20, 16.37)
+    >>> closer_city(41.29, 174.78, bucharest, vienna)
+    'Bucharest'
+    """
+    given = lat, lon
+    if distance(given, city_a) < distance(given, city_b):
+        return city_a
+    elif distance(given, city_a) >= distance(given, city_b):
+        return city_b
 
 # def check_city_abstraction():
 #     """
